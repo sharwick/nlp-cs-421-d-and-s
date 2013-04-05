e4d8ff3cbe1332b0d2b7a020f68a710b263bf63b
Shannon Harwick <sharwick@gmail.com>
2013-04-05 10:16:34 -0500

    Fix the call to POS tagger.  Test the new method by printing results by sentence and by WORD/POS combo.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.stanford.nlp.ling.Sentence;
+import edu.stanford.nlp.ling.TaggedWord;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+
 
 public class Main {
 
@@ -14,7 +25,25 @@ public class Main {
 		// Test 3a
 		Test.testScore(id, col3a, "Part 3a");
 		
-		Functions.runPOSTagger("./essay-corpus/1.txt");
+		
+		// Running POS tagger: returns an array of arrayLists.  
+		//		Elements of the array are sentences
+		//		Elements in each array list are WORD/POS combos (e.g., "the/DT")
+		//		The tagger is not good at finding the end of sentence if a period is missing
+		
+		String model = "./stanford-postagger-full-2012-11-11/models/wsj-0-18-left3words.tagger";
+		String filename = "./essay-corpus/1.txt";
+		ArrayList<TaggedWord>[] output = Tagger.RunTagger(model, filename);
+		
+		// Print the elements of the array and then the individual elements of each ArrayList
+		for(int i=0; i<output.length; i++) {
+			System.out.println(output[i].toString());
+			System.out.println("Sentence " + i);
+			for (int j=0; j<output[i].size(); j++) {
+				System.out.println( output[i].get(j));
+			}
+		}
+		
 	}
 
 }
--- src/Tagger.java
+++ src/Tagger.java
@@ -1,6 +1,6 @@
-
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,19 +11,57 @@ import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
 class Tagger {
 
+	/*
   private Tagger() {}
 
+  
   public static void main(String[] args) throws Exception {
     if (args.length != 2) {
       System.err.println("usage: java TaggerDemo modelFile fileToTag");
       return;
     }
+  */ 
+	/*
     MaxentTagger tagger = new MaxentTagger(args[0]);
     List<List<HasWord>> sentences = MaxentTagger.tokenizeText(new BufferedReader(new FileReader(args[1])));
     for (List<HasWord> sentence : sentences) {
       ArrayList<TaggedWord> tSentence = tagger.tagSentence(sentence);
       System.out.println(Sentence.listToString(tSentence, false));
     }
-  }
+    */
+	
+	
+    public static ArrayList<TaggedWord>[] RunTagger(String model, String inFile) {
+    	try{
+    		
+    		
+    		MaxentTagger tagger = new MaxentTagger(model);
+        	List<List<HasWord>> sentences = MaxentTagger.tokenizeText(new BufferedReader(new FileReader(inFile)));
+        	
+        	ArrayList<TaggedWord>[] returnArray;
+        	ArrayList<TaggedWord> temp;
+        	//Class<ArrayList<TaggedWord>> clazz = temp.getClass();
+        	//returnArray = (ArrayList<TaggedWord>[]) Array.newInstance(clazz.getComponentType(), sentences.size()); //new ArrayList<TaggedWord>[sentences.size()];
+        	returnArray = new ArrayList[sentences.size()];
+        	
+        	int i=0;
+            for (List<HasWord> sentence : sentences) {
+              ArrayList<TaggedWord> tSentence = tagger.tagSentence(sentence);
+              System.out.println(Sentence.listToString(tSentence, false));
+              
+              
+              returnArray[i++] = tSentence;
+            }	
+            
+            return returnArray;
+    	} catch (Exception e) {
+    		System.err.println("ERROR: " + e);
+    	}
+    	
+    	return null;
+    	
+    }
+    
+   
 
 }
